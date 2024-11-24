0637ee93f6f715b9c855ed3ed1bfcb8149eb9fec
Alex <massalex0209@gmail.com>
2024-11-25 00:57:04 +0300

    admin request fixes

--- server/static/js/found_animals.js
+++ server/static/js/found_animals.js
@@ -1,26 +1,25 @@
 // Получаем элементы
-const lostAnimalRequests = document.getElementById('foundAnimalsContainer');
+const lostAnimalRequests = document.getElementById("foundAnimalsContainer");
 // console.log(document);
 // Обработчики для каждой заявки
-const requests = lostAnimalRequests.getElementsByClassName('found-animal');
+const requests = lostAnimalRequests.getElementsByClassName("found-animal");
 
 for (let request of requests) {
-    // Обработчик для удаления заявки
-    const deleteButton = request.querySelector('.delete-button');
-    deleteButton.addEventListener('click', function () {
-        id = request.id
-        fetch("http://194.87.140.79:5000/admin/delete-form/" + id + "/");
-        lostAnimalRequests.removeChild(request);
+  // Обработчик для удаления заявки
+  const deleteButton = request.querySelector(".delete-button");
+  deleteButton.addEventListener("click", function () {
+    id = request.id;
+    fetch("http://194.87.140.79:8080/admin/delete-form/" + id + "/");
+    lostAnimalRequests.removeChild(request);
+  });
 
-    });
-
-    // Обработчик для отметки заявки как выполненной
-    const checkbox = request.querySelector('.request-checkbox');
-    checkbox.addEventListener('change', function () {
-        if (checkbox.checked) {
-            id = request.id
-            fetch("http://194.87.140.79:5000/admin/change-form/" + id + "/");
-            lostAnimalRequests.removeChild(request);
-        }
-    });
+  // Обработчик для отметки заявки как выполненной
+  const checkbox = request.querySelector(".request-checkbox");
+  checkbox.addEventListener("change", function () {
+    if (checkbox.checked) {
+      id = request.id;
+      fetch("http://194.87.140.79:8080/admin/change-form/" + id + "/");
+      lostAnimalRequests.removeChild(request);
+    }
+  });
 }
--- server/static/js/lost_animals.js
+++ server/static/js/lost_animals.js
@@ -1,26 +1,25 @@
 // Получаем элементы
-const lostAnimalRequests = document.getElementById('lostAnimalRequests');
+const lostAnimalRequests = document.getElementById("lostAnimalRequests");
 
 // Обработчики для каждой заявки
-const requests = lostAnimalRequests.getElementsByClassName('lost-animal');
+const requests = lostAnimalRequests.getElementsByClassName("lost-animal");
 
 for (let request of requests) {
-    // Обработчик для удаления заявки
-    const deleteButton = request.querySelector('.delete-button');
-    deleteButton.addEventListener('click', function () {
-        id = request.id
-        fetch("http://194.87.140.79:5000/admin/delete-form/" + id + "/");
-        lostAnimalRequests.removeChild(request);
+  // Обработчик для удаления заявки
+  const deleteButton = request.querySelector(".delete-button");
+  deleteButton.addEventListener("click", function () {
+    id = request.id;
+    fetch("http://194.87.140.79:8080/admin/delete-form/" + id + "/");
+    lostAnimalRequests.removeChild(request);
+  });
 
-    });
-
-    // Обработчик для отметки заявки как выполненной
-    const checkbox = request.querySelector('.request-checkbox');
-    checkbox.addEventListener('change', function () {
-        if (checkbox.checked) {
-            id = request.id
-            fetch("http://194.87.140.79:5000/admin/change-form/" + id + "/");
-            lostAnimalRequests.removeChild(request);
-        }
-    });
+  // Обработчик для отметки заявки как выполненной
+  const checkbox = request.querySelector(".request-checkbox");
+  checkbox.addEventListener("change", function () {
+    if (checkbox.checked) {
+      id = request.id;
+      fetch("http://194.87.140.79:8080/admin/change-form/" + id + "/");
+      lostAnimalRequests.removeChild(request);
+    }
+  });
 }
