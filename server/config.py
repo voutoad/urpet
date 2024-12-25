ebc4e175c1862781cae74076d9eb806c87e775ca
alex <massalex0209@gmail.com>
2024-12-25 03:13:33 +0300

    fix

--- server/config.py
+++ server/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent
 
-SMTP_URL = os.getenv('SMTP_SSL') + os.getenv('SMTP_PORT')
+SMTP_URL = str(os.getenv('SMTP_SSL')) + str(os.getenv('SMTP_PORT'))
 SMTP_PASSWORD = os.getenv('SMTP_PASSWORD')
 SECRET_KEY = os.getenv('SECRET_KEY')
 DB_URL = os.getenv('DATABASE')
